2ba82404bfc661615701d9985f0a37e3dbb2f9a9
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2022-12-29 14:08:41 +0700

    Update counter.blade.php

--- resources/views/counter.blade.php
+++ resources/views/counter.blade.php
@@ -1,4 +1,9 @@
 <center>
+    <style>
+        #besucherzaehler2{
+            display: none;
+        }
+    </style>
     <p class="text-dark text-center d-none" style="display:none;margin-top:-10px;margin-bottom:0px;font-size:11px">ข้อมูลตั้งแต่วันที่ 27/12/2022</p>
     <div class="นับ ">
     <a style="display:none;" class="d-none" href="https://www.freecounterstat.com" title="website counter"><img src="https://counter5.optistats.ovh/private/freecounterstat.php?c=ycwbm8ba3a3g3wslbaybeks5qyrt4crd" border="0" title="website counter" alt="website counter"></a>

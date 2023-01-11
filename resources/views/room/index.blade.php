6186cef2fdd01e25042a3c2157205274bc947403
Lomtale <deer11485@gmail.com>
2023-01-11 11:28:28 +0700

    แก้ไข flex หน้า room index

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -61,47 +61,74 @@
                     </div>
                 </div>
                 <hr width="97%">
-                @foreach($room as $item)
-                <div class="card col-4 ">
-                    <img class="card-img-top mt-3" src="{{asset('/img/logo_mithcare/portrait-volunteer-who-organized-donations-charity.jpg')}}" width="100%" height="150px" style="object-fit: cover;" alt="Card image cap">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-6">
-                                <a href="{{ url('/room/' . $item->id) }}" class="btn-old btn-info btn-sm btn-block main-shadow main-radius">                                
-                                    <i class="fa-solid fa-magnifying-glass"></i> รายละเอียด        
-                                </a>
-                            </div>
-                            <div class="col-6">
-                                <button class="btn-old btn-success btn-sm btn-block main-shadow main-radius">ตารางนัด</button>
-                            </div>
-                        </div>
-                        <p class="text-center main-shadow main-radius btn-block mt-2 p-2" style="background-color:#407CAB; color:#ffffff; font-size:18px">{{$item->name}}</p>
-                        <p style="font-size: 13px;">เจ้าของบ้าน : {{$item->user->name}}</p>
-                        <hr>
-                        @if(Auth::user()->id == $item->owner_id)
-             
-                        <div class="row">
-                            <div class="col-12">
-                                <i data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample" class="fa-solid fa-caret-down" style="font-size: 30px; float: right"></i>
-                            </div>
-                            <div class="collapse col-12" id="collapseExample">
-                                <div class="row mr-2">
+                <div class="row">
+                
 
-                                    <a href="{{ url('/room/' . $item->id . '/edit') }}" title="Edit Room"><button class="btn-old btn-primary btn-sm">
-                                            <i class="fa-solid fa-pen-to-square"></i></button></a>
-
-                                    <form method="POST" action="{{ url('/room' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                        {{ method_field('DELETE') }}
-                                        {{ csrf_field() }}
-                                        <button type="submit" class="btn-old btn-danger btn-sm" title="Delete Room" onclick="return confirm('ต้องการลบใช่หรือไม่')"><i class="fa-solid fa-trash"></i></button>
-                                    </form>
+                    @foreach($room as $item)
+                    <div class="col-md-4 col-sm-12">
+                        <div class="card  product-item">
+                            <img class="card-img-top p-3 " src="{{asset('/img/logo_mithcare/home-background.png')}}" width="100%" height="150px" style="object-fit: cover;" alt="Card image cap">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-6">
+                                        <a href="{{ url('/room/' . $item->id) }}" class="btn-old btn-info btn-sm btn-block main-shadow main-radius">                                
+                                            <!-- <i class="fa-solid fa-magnifying-glass"></i>  -->
+                                            รายละเอียด        
+                                        </a>
+                                    </div>
+                                    <div class="col-6">
+                                        <button class="btn-old btn-success btn-sm btn-block main-shadow main-radius">ตารางนัด</button>
+                                    </div>
+                                    <div class="col-12">
+                                        <hr>
+                                            <p class="pricing__title text-center mt-2 p-2 h3" style="color: #4170A2;">{{$item->name}}</ย>
+                                            <p style="font-size: 20px;">เจ้าของบ้าน : </p>
+                                        <hr>
+                                    </div>
                                 </div>
-                            </div>
-                        </div>               
-                        @endif
-                    </div><!--  card-body -->
-                </div><!--  card -->
-                @endforeach
+                                    
+                                @if(Auth::user()->id == $item->owner_id)
+                    
+                                <div class="row">
+
+                                    
+                                    <div class="col-12">
+                                        <a data-toggle="collapse" href="#collapseExample{{$item->id}}" aria-expanded="false" aria-controls="collapseExample{{$item->id}}" class="btn-old btn-info text-white" style="float: right;">
+                                          เพิ่มเติม <i class="fa-solid fa-caret-down"></i> 
+                                        </a>
+                                        
+                                    </div>
+                                    
+                                    <div class="collapse  " id="collapseExample{{$item->id}}"> 
+                                        <!-- <br><br>-->
+                                        <!-- <div class="col-12"></div> -->
+                                        <div class="row ">
+                                            <div class="col-6 ">
+                                                <a class="btn-old btn-primary btn-sm main-shadow main-radius" href="{{ url('/room/' . $item->id . '/edit') }}" title="Edit Room">
+                                                        <i class="fa-solid fa-pen-to-square"></i> แก้ไขบ้าน
+                                                </a>
+                                            </div>       
+                                            <div class="col-6 ">
+                                                <form method="POST" action="{{ url('/room' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+                                                    <button type="submit" class="btn-old btn-danger btn-sm main-shadow main-radius" title="Delete Room" onclick="return confirm('ต้องการลบใช่หรือไม่')">
+                                                        <i class="fa-solid fa-trash"></i> ลบบ้าน
+                                                    </button>
+                                                </form>
+                                            </div>    
+                                        </div>
+
+                                    </div>
+
+                                    
+                                </div>               
+                                @endif
+                            </div><!--  card-body -->
+                        </div><!--  card -->
+                    </div>
+                    @endforeach
+                </div>
                 <div class="pagination-wrapper"> {!! $room->appends(['search' => Request::get('search')])->render() !!} </div>
                
             </div> <!-- contact-panel -->
--- resources/views/room/show.blade.php
+++ resources/views/room/show.blade.php
@@ -3,69 +3,99 @@
 @section('content')
 
 <section class="page-title page-title-layout5">
-        <div class="bg-img"><img src="{{asset('/img/พื้นหลัง/พื้นหลัง-05.png')}}" width="90%" alt="background"></div>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <h1 class="pagetitle__heading">deer</h1>
-                    <nav>
-                        <!-- แสดงเฉพาะคอม -->
-                        <div class="d-none d-lg-block">
-                            <ol class=" breadcrumb mb-0 ">
-                                <li class="breadcrumb-item"><a href="{{ url('/') }}" style="font-size: 30px;">หน้าแรก</a></li>
-                                <li class="breadcrumb-item"><a href="{{ url('/room') }}" style="font-size: 30px;">บ้านของฉัน</a></li>
-                                <li class="breadcrumb-item"><a href="#" style="font-size: 30px;">{{$room->name}}</a></li>
-                            </ol>
-                        </div> <!--d-none d-lg-block -->
-                        <!-- แสดงเฉพาะมือถือ -->
-                        <div class="d-block d-md-none">
-                            <ol class=" breadcrumb mb-0">
-                                <li class="breadcrumb-item"><a href="{{ url('/') }}" style="font-size: 20px;">หน้าแรก</a></li>
-                                <li class="breadcrumb-item"><a href="{{ url('/room') }}" style="font-size: 20px;">บ้านของฉัน</a></li>
-                                <li class="breadcrumb-item"><a href="#" style="font-size: 20px;">{{$room->name}}</a></li>
-                            </ol>
-                        </div> <!--d-block d-md-none -->
-                    </nav>
-                </div><!-- /.col-12 -->
-            </div><!-- /.row -->
-        </div><!-- /.container -->
+    <div class="bg-img"><img src="{{asset('/img/พื้นหลัง/พื้นหลัง-05.png')}}" width="90%" alt="background"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="pagetitle__heading">deer</h1>
+                <nav>
+                    <!-- แสดงเฉพาะคอม -->
+                    <div class="d-none d-lg-block">
+                        <ol class=" breadcrumb mb-0 ">
+                            <li class="breadcrumb-item"><a href="{{ url('/') }}" style="font-size: 30px;">หน้าแรก</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('/room') }}" style="font-size: 30px;">บ้านของฉัน</a></li>
+                            <li class="breadcrumb-item"><a href="#" style="font-size: 30px;">{{$room->name}}</a></li>
+                        </ol>
+                    </div> <!--d-none d-lg-block -->
+                    <!-- แสดงเฉพาะมือถือ -->
+                    <div class="d-block d-md-none">
+                        <ol class=" breadcrumb mb-0">
+                            <li class="breadcrumb-item"><a href="{{ url('/') }}" style="font-size: 20px;">หน้าแรก</a></li>
+                            <li class="breadcrumb-item"><a href="{{ url('/room') }}" style="font-size: 20px;">บ้านของฉัน</a></li>
+                            <li class="breadcrumb-item"><a href="#" style="font-size: 20px;">{{$room->name}}</a></li>
+                        </ol>
+                    </div> <!--d-block d-md-none -->
+                </nav>
+            </div><!-- /.col-12 -->
+        </div><!-- /.row -->
+    </div><!-- /.container -->
 </section><!-- /.page-title -->
 
 <section class="page-title page-title-layout5">
     <div class="container">
         <div class="row">
-    
+
             <div class="contact-panel col-md-12 mb-2">
                 <div class="row">
-
-                    <h3 >บ้าน {{ $room->name }} </h3>
-                    <a href="{{ url('/room/' . $room->id . '/edit') }}" title="Edit Room"><button class="btn-old btn-primary btn-sm main-shadow main-radius m-2"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-                </div>
-                    <div class="h5">
+                    <h3>บ้าน {{ $room->name }} </h3>
+                    <div>
+                        <a href="{{ url('/room/' . $room->id . '/edit') }}" title="Edit Room">
+                            <button class="btn-old btn-primary btn-sm main-shadow main-radius m-2">
+                                <i class="fa-solid fa-pen-to-square"></i> แก้ไขบ้าน
+                            </button>
+                        </a>
                         <form method="POST" action="{{ url('room' . '/' . $room->id) }}" accept-charset="UTF-8" style="display:inline">
                             {{ method_field('DELETE') }}
                             {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-sm main-shadow main-radius" title="Delete Room" onclick="return confirm('ต้องการลบใช่ไหม')"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                            <button type="submit" class="btn-old btn-danger btn-sm main-shadow main-radius m-2" title="Delete Room" onclick="return confirm('ต้องการลบใช่ไหม')">
+                                <i class="fa-solid fa-trash"></i> ลบบ้าน
+                            </button>
                         </form>
-                        <br/>
-                        <br/>
+                    </div>
+                </div>
+                <div class="h5">
+                    <br />
+                    <br />
 
-                        <div class="table-responsive">
-                            <table class="table">
-                                <tbody>
-                                    <tr>
-                                        <th>รหัสค้นหาบ้าน</th><td>{{ $room->id }}</td>
-                                    </tr>
-                                    <tr>
-                                        <th> Name </th>
-                                    </tr>
-                                    <td> {{ $room->name }} </td></tr>
-                                </tbody>
-                            </table>
+                    <div class="collg-12 col-md-9">
+                        <div class="pricing-widget-layout2 mb-70 product-item">                   
+                            <ul class="pricing__list list-unstyled mb-0">
+                                <li><span>รหัสค้นหาบ้าน</span><span class="price">{{ $room->id }}</span></li>
+                                <li><span>ชื่อบ้าน</span><span class="price">{{ $room->name }}</span></li>
+                                <li><span>เจ้าของบ้าน</span><span class="price">{{ $room->user->name }}</span></li>                          
+                            </ul>
                         </div>
                     </div>
-                    <a href="#" onclick="goBack()"><button class="btn btn-info btn-sm main-shadow main-radius"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                
+
+                    <div class="collg-12 col-md-9 ">
+                        <div class="pricing-widget-layout2 mb-70 product-item">    
+                                <h4>สมาชิกในบ้าน</h4>               
+                            <ul class="pricing__list list-unstyled mb-0">
+                                <li><span>1</span><span class="price">คุณ ..........</span></li>
+                                <li><span>2</span><span class="price">คุณ ..........</span></li>
+                                <li><span>3</span><span class="price">คุณ ..........</span></li>                          
+                            </ul>
+                        </div>
+                    </div>
+
+                    <!-- <div class="table-responsive">
+                        <table class="table">
+                            <tbody>
+                                <tr>
+                                    <th>รหัสค้นหาบ้าน</th>
+                                    <td>{{ $room->id }}</td>
+                                </tr>
+                                <tr>
+                                    <th> Name </th>
+                                </tr>
+                                <td> {{ $room->name }} </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div> -->
+                </div>
+                <a href="#" onclick="goBack()"><button class="btn-old btn-info btn-sm main-shadow main-radius"><i class="fa fa-arrow-left" aria-hidden="true"></i> กลับ</button></a>
+
             </div>
         </div>
     </div>

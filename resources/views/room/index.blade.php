49c3b4564086bcc1098911d7b371f489eadb4071
luxlnw171041 <54930672+luxlnw171041@users.noreply.github.com>
2023-01-11 17:11:04 +0700

    calendar

--- resources/views/room/index.blade.php
+++ resources/views/room/index.blade.php
@@ -1,8 +1,76 @@
 @extends('layouts.mithcare')
 
 @section('content')
-
-
+<link href="assets/plugins/fullcalendar/css/main.min.css" rel="stylesheet" />
+<link href="{{ asset('mithcare/js/fullcalendar/css/main.min.css') }}" rel="stylesheet">
+	    <div id='calendar'></div>
+    <script src="{{ asset('mithcare/js/fullcalendar/js/main.min.js') }}"></script>
+	<script>
+		document.addEventListener('DOMContentLoaded', function () {
+			var calendarEl = document.getElementById('calendar');
+			var calendar = new FullCalendar.Calendar(calendarEl, {
+				headerToolbar: {
+					left: 'prev,next today',
+					center: 'title',
+					right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+				},
+				initialView: 'dayGridMonth',
+				initialDate: '2020-09-12',
+				navLinks: true, // can click day/week names to navigate views
+				selectable: true,
+				nowIndicator: true,
+				dayMaxEvents: true, // allow "more" link when too many events
+				editable: true,
+				selectable: true,
+				businessHours: true,
+				dayMaxEvents: true, // allow "more" link when too many events
+				events: [{
+					title: 'All Day Event',
+					start: '2020-09-01',
+				}, {
+					title: 'Long Event',
+					start: '2020-09-07',
+					end: '2020-09-10'
+				}, {
+					groupId: 999,
+					title: 'Repeating Event',
+					start: '2020-09-09T16:00:00'
+				}, {
+					groupId: 999,
+					title: 'Repeating Event',
+					start: '2020-09-16T16:00:00'
+				}, {
+					title: 'Conference',
+					start: '2020-09-11',
+					end: '2020-09-13'
+				}, {
+					title: 'Meeting',
+					start: '2020-09-12T10:30:00',
+					end: '2020-09-12T12:30:00'
+				}, {
+					title: 'Lunch',
+					start: '2020-09-12T12:00:00'
+				}, {
+					title: 'Meeting',
+					start: '2020-09-12T14:30:00'
+				}, {
+					title: 'Happy Hour',
+					start: '2020-09-12T17:30:00'
+				}, {
+					title: 'Dinner',
+					start: '2020-09-12T20:00:00'
+				}, {
+					title: 'Birthday Party',
+					start: '2020-09-13T07:00:00'
+				}, {
+					title: 'Click for Google',
+					url: 'http://google.com/',
+					start: '2020-09-28'
+				}]
+			});
+			calendar.render();
+		});
+	</script>
 <section class="page-title page-title-layout5">
     <div class="bg-img"><img src="{{asset('/img/พื้นหลัง/พื้นหลัง-05.png')}}" width="90%" alt="background"></div>
     <div class="container">

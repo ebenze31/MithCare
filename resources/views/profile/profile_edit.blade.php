c6d58aedb70df1af2a1eb618dd6c4ff9cbc6ba76
Lomtale <deer11485@gmail.com>
2023-01-25 17:25:09 +0700

    profile edit

--- resources/views/profile/profile_edit.blade.php
+++ resources/views/profile/profile_edit.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
      <!-- ========================
-       page title 
+       page title
     =========================== -->
     <section class="page-title page-title-layout5">
       <div class="bg-img"><img src="{{asset('/img/พื้นหลัง/พื้นหลัง-05.png')}}" width="90%" alt="background"></div>
@@ -13,11 +13,11 @@
             <h1 class="pagetitle__heading" >User Name</h1>
             <nav>
                 <!-- แสดงเฉพาะคอม -->
-               <div class="d-none d-lg-block"> 
+               <div class="d-none d-lg-block">
                   <ol class=" breadcrumb mb-0 ">
                     <li class="breadcrumb-item"><a href="{{ url('/') }}" style="font-size: 30px;">หน้าแรก</a></li>
                     <li class="breadcrumb-item"><a href="{{ url('/profile') }}" style="font-size: 30px;">โปรไฟล์</a></li>
-                    <li class="breadcrumb-item"><a href="{{ url('/profile/edit')}}" style="font-size: 30px;">แก้ไขโปรไฟล์</a></li>  
+                    <li class="breadcrumb-item"><a href="{{ url('/profile/edit')}}" style="font-size: 30px;">แก้ไขโปรไฟล์</a></li>
                   </ol>
                 </div> <!--d-none d-lg-block -->
 
@@ -26,7 +26,7 @@
                   <ol class=" breadcrumb mb-0">
                     <li class="breadcrumb-item"><a href="{{ url('/') }}" style="font-size: 20px;">หน้าแรก</a></li>
                     <li class="breadcrumb-item"><a href="{{ url('/profile') }}" style="font-size: 20px;">โปรไฟล์</a></li>
-                    <li class="breadcrumb-item"><a href="{{ url('/profile/edit')}}" style="font-size: 30px;">แก้ไขโปรไฟล์</a></li>   
+                    <li class="breadcrumb-item"><a href="{{ url('/profile/edit')}}" style="font-size: 30px;">แก้ไขโปรไฟล์</a></li>
                   </ol>
                 </div> <!--d-block d-md-none -->
             </nav>
@@ -43,11 +43,15 @@
               <div class="widget widget-member">
                 <div class="member mb-0">
                   <div class="member__img">
-                    <img src="{{asset('/img/logo_mithcare/portrait-volunteer-who-organized-donations-charity.jpg')}}" alt="member img">
+                    @if (!empty($user->photo))
+                        <img src="{{ url('storage/'.$user->photo )}}" alt="member img" height="300px" width="100%">
+                     @else
+                        <img src="https://www.viicheck.com/Medilab/img/icon.png" alt="member img" height="300px" width="100%">
+                    @endif
                   </div><!-- /.member-img -->
                   <div class="member__info">
                     <h2 class="member__name text-center"><a href="#" style="font-size: 30px;">{{$user->full_name}}</a></h2>
-                  
+
                   </div><!-- /.member-info -->
                 </div><!-- /.member -->
               </div><!-- /.widget-member -->
@@ -57,50 +61,50 @@
 
                         <div class="widget widget-help bg-overlay bg-overlay-primary-gradient main-shadow">
                           <div class="bg-img"><img src="assets/images/banners/5.jpg" alt="background"></div>
-                          <div class="widget-content">                           
-                            <h2 class="widget__title" style="font-size: 30px;">บัตร 1</h2>   
+                          <div class="widget-content">
+                            <h2 class="widget__title" style="font-size: 30px;">บัตร 1</h2>
                             <div class="form-group ">
                                   <input class="form-control" name="health_card_1" type="file" id="health_card_1" value="" >
-                            </div>                                       
-                          </div><!-- /.widget-content -->
-                        </div><!-- /.widget-help -->
-
-                        <div class="widget widget-help bg-overlay bg-overlay-primary-gradient main-shadow">
-                          <div class="bg-img"><img src="assets/images/banners/5.jpg" alt="background"></div>
-                          <div class="widget-content">                          
-                            <h2 class="widget__title" style="font-size: 30px;">บัตร 2</h2>
-                            <div class="form-group ">
-                                  <input class="form-control" name="health_card_2" type="file" id="health_card_2" value="" >
-                            </div>   
+                            </div>
                           </div><!-- /.widget-content -->
                         </div><!-- /.widget-help -->
 
                         <div class="widget widget-help bg-overlay bg-overlay-primary-gradient main-shadow">
                           <div class="bg-img"><img src="assets/images/banners/5.jpg" alt="background"></div>
                           <div class="widget-content">
-                           
+                            <h2 class="widget__title" style="font-size: 30px;">บัตร 2</h2>
+                            <div class="form-group ">
+                                  <input class="form-control" name="health_card_2" type="file" id="health_card_2" value="" >
+                            </div>
+                          </div><!-- /.widget-content -->
+                        </div><!-- /.widget-help -->
+
+                        <div class="widget widget-help bg-overlay bg-overlay-primary-gradient main-shadow">
+                          <div class="bg-img"><img src="assets/images/banners/5.jpg" alt="background"></div>
+                          <div class="widget-content">
+
                             <h2 class="widget__title" style="font-size: 30px;">บัตร 3</h2>
                             <div class="form-group ">
                                   <input class="form-control" name="health_card_3" type="file" id="health_card_3" value="" >
-                            </div>   
+                            </div>
                           </div><!-- /.widget-content -->
                         </div><!-- /.widget-help -->
 
                       </aside><!-- /.sidebar -->
                     </div><!-- /.col-lg-4 -->
                     <div class="col-sm-12 col-md-12 col-lg-8">
-                      
-                      <ul class="details-list list-unstyled mb-60 mt-40">    
+
+                      <ul class="details-list list-unstyled mb-60 mt-40">
 
                             @include ('profile.profile_form', ['formMode' => 'edit'])
 
                 </form> <!--form edit_profile -->
             </ul><!-- /.widget-content -->
-             
-           
 
-          
-            
+
+
+
+
           </div><!-- /.col-lg-8 -->
         </div><!-- /.row -->
       </div><!-- /.container -->

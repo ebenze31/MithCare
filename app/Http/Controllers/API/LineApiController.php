c437812b1c8c7e53ea7ed7e80eb8b080a5c06684
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-01-30 15:13:15 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
 use App\Models\Mylog;
 
 class LineApiController extends Controller
@@ -20,5 +19,4 @@ class LineApiController extends Controller
         ];
         Mylog::create($data);
     }
-
 }

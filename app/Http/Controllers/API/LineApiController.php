42819d1963e60698d93429213feb8f433f0b9e17
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-01-30 16:41:08 +0700

    send line

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Mylog;
+use App\Models\LineMessagingAPI;
 
 
 class LineApiController extends Controller
@@ -20,28 +21,56 @@ class LineApiController extends Controller
         MyLog::create($data);  
 
         //GET ONLY FIRST EVENT
-        // $event = $requestData["events"][0];
+        $event = $requestData["events"][0];
 
-        // switch($event["type"]){
-        //     case "message" : 
-        //         $this->messageHandler($event);
-        //         break;
-        //     case "postback" : 
-        //         $this->postbackHandler($event);
-        //         break;
-        //     case "join" :
-        //         $this->save_group_line($event);
-        //         break;
-        //     case "follow" :
-        //         $this->user_follow_line($event);
-        //         DB::table('users')
-        //             ->where([ 
-        //                     ['type', 'line'],
-        //                     ['provider_id', $event['source']['userId']],
-        //                     ['status', "active"] 
-        //                 ])
-        //             ->update(['add_line' => 'Yes']);
-        //         break;
-        // }
+        switch($event["type"]){
+            case "message" : 
+                $this->messageHandler($event);
+                break;
+            case "postback" : 
+                $this->postbackHandler($event);
+                break;
+            case "join" :
+                $this->save_group_line($event);
+                break;
+            case "follow" :
+                $this->user_follow_line($event);
+                DB::table('users')
+                    ->where([ 
+                            ['type', 'line'],
+                            ['provider_id', $event['source']['userId']],
+                            ['status', "active"] 
+                        ])
+                    ->update(['add_line' => 'Yes']);
+                break;
+        }
     }
+
+    function messageHandler($event){
+
+        switch($event["message"]["type"]){
+            case "text" : 
+                $this->textHandler($event);
+                break;
+        }
+
+    }
+
+     public function textHandler($event)
+    {
+        $line = new LineMessagingAPI();
+        $text = $event["message"]["text"] ;
+
+        switch( $text )
+        {     
+            case "ทดสอบ" :  
+                $line->replyToUser($event, "test");
+                break;
+            case "สมัครสมาชิก" :  
+                $line->replyToUser($event, "register");
+                break;
+        }   
+        
+    }
+
 }
---
+++ app/Models/LineMessagingAPI.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\DB;
+use App\Models\Mylog;
+
+use App\User;
+// use Carbon\Carbon;
+
+class LineMessagingAPI extends Model
+{
+    // public $channel_access_token = env('CHANNEL_ACCESS_TOKEN');
+
+    public function reply_success($event , $data_postback)
+    {
+        $data_Text_topic = [
+            "ระบบได้รับการตอบกลับของท่านแล้ว ขอบคุณค่ะ",
+        ];
+        
+        $data_topic = $this->language_for_user($data_Text_topic, $event["source"]['userId']);
+
+        $template_path = storage_path('../public/json/text_success.json');   
+
+        $string_json = file_get_contents($template_path);
+        $string_json = str_replace("ระบบได้รับการตอบกลับของท่านแล้ว ขอบคุณค่ะ",$data_topic[0],$string_json);
+        $messages = [ json_decode($string_json, true) ];
+
+
+        $body = [
+            "replyToken" => $event["replyToken"],
+            "messages" => $messages,
+        ];
+
+        $opts = [
+            'http' =>[
+                'method'  => 'POST',
+                'header'  => "Content-Type: application/json \r\n".
+                            'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
+                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                //'timeout' => 60
+            ]
+        ];
+                            
+        $context  = stream_context_create($opts);
+        //https://api-data.line.me/v2/bot/message/11914912908139/content
+        $url = "https://api.line.me/v2/bot/message/reply";
+        $result = file_get_contents($url, false, $context);
+
+        //SAVE LOG
+        $data = [
+            "title" => "ระบบได้รับการตอบกลับของท่านแล้ว ขอบคุณค่ะ",
+            "content" => "reply Success",
+        ];
+        MyLog::create($data);
+        return $result;
+
+    }
+
+    public function replyToUser($event, $message_type)
+    {   
+        switch ($message_type) {
+            case 'test':
+                $template_path = storage_path('../public/json/text.json');   
+                $string_json = file_get_contents($template_path);
+
+                $string_json = str_replace("hello" , "ทดสอบอะไร" ,$string_json);
+                break;
+            case 'register':
+                $template_path = storage_path('../public/json/text.json');   
+                $string_json = file_get_contents($template_path);
+
+                $string_json = str_replace("hello" , "ยังไม่ได้เปิดครับ" ,$string_json);
+                break;
+            
+            default:
+                $template_path = storage_path('../public/json/text.json');   
+                $string_json = file_get_contents($template_path);
+
+                $string_json = str_replace("hello" , "สวัสดีครับคุณพรี๊" ,$string_json);
+                break;
+        }
+
+        $messages = [ json_decode($string_json, true) ];
+
+        $body = [
+            "replyToken" => $event["replyToken"],
+            "messages" => $messages,
+        ];
+
+        $opts = [
+            'http' =>[
+                'method'  => 'POST',
+                'header'  => "Content-Type: application/json \r\n".
+                            'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
+                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                //'timeout' => 60
+            ]
+        ];
+                            
+        $context  = stream_context_create($opts);
+        //https://api-data.line.me/v2/bot/message/11914912908139/content
+        $url = "https://api.line.me/v2/bot/message/reply";
+        $result = file_get_contents($url, false, $context);
+
+        //SAVE LOG
+        $data = [
+            "title" => "reply test",
+            "content" => "reply test",
+        ];
+        MyLog::create($data);
+        return $result;
+
+    }
+
+
+}

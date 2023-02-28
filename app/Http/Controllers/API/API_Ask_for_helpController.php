67974a476cccd07e997946c29c0464ba1ed0519a
Lomtale <deer11485@gmail.com>
2023-02-28 14:57:50 +0700

    v4

--- app/Http/Controllers/API/API_Ask_for_helpController.php
+++ app/Http/Controllers/API/API_Ask_for_helpController.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Facades\DB;
 use App\Models\Ask_for_help;
 use Illuminate\Support\Facades\Auth;
 use App\User;
+use App\Models\Group_line;
+use App\Models\Mylog;
 
 class API_Ask_for_helpController extends Controller
 {
@@ -46,9 +48,9 @@ class API_Ask_for_helpController extends Controller
         $ask_for_help = Ask_for_help::create($requestData);
 
          // หา $id_sos_map
-         foreach ($ask_for_help as $latest) {
-             $id_sos_map = $latest->id;
-         }
+        foreach ($ask_for_help as $latest) {
+            $id_sos_map = $latest->id;
+        }
 
          switch ($requestData['content']) {
              case 'help_by_partner':
@@ -72,5 +74,59 @@ class API_Ask_for_helpController extends Controller
 
         return $data_sos ;
     }
+
+    public function send_Line_To_Group_SOS($data_sos,$id_sos_map){
+
+        $sosTo = "mithcare";
+        $data_groupline = Group_line::where('owner','mithcare')->first();
+        $text_at = "@";
+         // sendto Provider_id
+         $sendto = User::where('id','=',$data_sos['user_id'])->first();
+         $provider_id = $sendto->provider_id;
+
+        $template_path = storage_path('../public/json/flex_sos_from_ask_for_help.json');
+        $string_json = file_get_contents($template_path);
+        // กรณีเป็นนัดหมายของผู้ป่วยlv2 ให้แสดงชื่อผู้ป่วย แทนคนสร้าง
+
+        $string_json = str_replace("User_name",$data_sos['name_user'],$string_json);
+        $string_json = str_replace("จังหวัด",$data_sos['province'],$string_json);
+        $string_json = str_replace("อำเภอ",$data_sos['district'],$string_json);
+        $string_json = str_replace("ตำบล",$data_sos['sub_district'],$string_json);
+        $string_json = str_replace("รายละเอียดที่อยู่",$data_sos['address'],$string_json);
+        $string_json = str_replace("gg_lat",$data_sos['lat'],$string_json);
+        $string_json = str_replace("gg_lng",$data_sos['lng'],$string_json);
+        $string_json = str_replace("gg_lat_mail",$text_at.$data_sos['lat'],$string_json);
+        $string_json = str_replace("0999999999",$sendto->phone,$string_json);
+
+        $string_json = str_replace("id_sos_map",$id_sos_map,$string_json);
+
+        $messages = [ json_decode($string_json, true) ];
+        $body = [
+            "to" => $data_groupline->groupId,
+            "messages" => $messages,
+        ];
+        $opts = [
+            'http' =>[
+                'method'  => 'POST',
+                'header'  => "Content-Type: application/json \r\n".
+                            'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
+                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                //'timeout' => 60
+            ]
+        ];
+
+        $context  = stream_context_create($opts);
+        $url = "https://api.line.me/v2/bot/message/push";
+        $result = file_get_contents($url, false, $context);
+
+        //SAVE LOG
+        $data = [
+            "title" => "ส่งไลน์",
+            "content" => json_encode($result, JSON_UNESCAPED_UNICODE),
+        ];
+
+        Mylog::Create($data);
+    }
+
 
 }
--- app/Http/Controllers/Ask_for_helpController.php
+++ app/Http/Controllers/Ask_for_helpController.php
@@ -185,58 +185,6 @@ class Ask_for_helpController extends Controller
 
     }
 
-    public function send_Line_To_Group_SOS($data_sos,$id_sos_map){
-
-        $sosTo = "mithcare";
-        $data_groupline = Group_line::where('owner','mithcare')->first();
-        $text_at = "@";
-         // sendto Provider_id
-         $sendto = User::where('id','=',$data_sos['user_id'])->first();
-         $provider_id = $sendto->provider_id;
-
-        $template_path = storage_path('../public/json/flex_sos_from_ask_for_help.json');
-        $string_json = file_get_contents($template_path);
-        // กรณีเป็นนัดหมายของผู้ป่วยlv2 ให้แสดงชื่อผู้ป่วย แทนคนสร้าง
-
-        $string_json = str_replace("User_name",$data_sos['name_user'],$string_json);
-        $string_json = str_replace("จังหวัด",$data_sos['province'],$string_json);
-        $string_json = str_replace("อำเภอ",$data_sos['district'],$string_json);
-        $string_json = str_replace("ตำบล",$data_sos['sub_district'],$string_json);
-        $string_json = str_replace("รายละเอียดที่อยู่",$data_sos['address'],$string_json);
-        $string_json = str_replace("gg_lat",$data_sos['lat'],$string_json);
-        $string_json = str_replace("gg_lng",$data_sos['lng'],$string_json);
-        $string_json = str_replace("gg_lat_mail",$text_at.$data_sos['lat'],$string_json);
-        $string_json = str_replace("0999999999",$sendto->phone,$string_json);
-
-        $string_json = str_replace("id_sos_map",$id_sos_map,$string_json);
-
-        $messages = [ json_decode($string_json, true) ];
-        $body = [
-            "to" => $data_groupline->groupId,
-            "messages" => $messages,
-        ];
-        $opts = [
-            'http' =>[
-                'method'  => 'POST',
-                'header'  => "Content-Type: application/json \r\n".
-                            'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
-                'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
-                //'timeout' => 60
-            ]
-        ];
-
-        $context  = stream_context_create($opts);
-        $url = "https://api.line.me/v2/bot/message/push";
-        $result = file_get_contents($url, false, $context);
-
-        //SAVE LOG
-        $data = [
-            "title" => "ส่งไลน์",
-            "content" => json_encode($result, JSON_UNESCAPED_UNICODE),
-        ];
-
-        Mylog::Create($data);
-    }
 
 
 

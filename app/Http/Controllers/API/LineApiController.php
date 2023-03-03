912a0f296a859098e134abda675c9216bbd0a170
Lomtale <deer11485@gmail.com>
2023-03-03 15:20:11 +0700

    Update LineApiController.php

--- app/Http/Controllers/API/LineApiController.php
+++ app/Http/Controllers/API/LineApiController.php
@@ -202,7 +202,7 @@ class LineApiController extends Controller
 
         $data_55 = [
             "title" => "User",
-            "content" => "ข้อมูลask_for_help :".$users->name,
+            "content" => $users,
         ];
         MyLog::create($data_55);
 

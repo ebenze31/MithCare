ba245a65a6d7bcb2629441ac65b82759b37adf3f
Lomtale <deer11485@gmail.com>
2023-03-14 12:32:30 +0700

    แก้ crontab

--- app/Console/Commands/Send_Appoint_Pill.php
+++ app/Console/Commands/Send_Appoint_Pill.php
@@ -65,7 +65,6 @@ class Send_Appoint_Pill extends Command
 
         for($i = 0; $i < count($ap_pill); $i++){
 
-
             // ค้นหา user_id สมาชิกในห้อง โดยหาจาก patient_id ที่ได้มา
             $data_members = Member_of_room::where('user_id',$ap_pill[$i]['patient_id'])->where('room_id',$ap_pill[$i]['room_id'])->first();
 

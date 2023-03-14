0c7d8d9d22513f8a661b20404813f8892643d248
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2023-03-14 17:06:51 +0700

    .

--- app/Console/Commands/Test_delete.php
+++ app/Console/Commands/Test_delete.php
@@ -50,10 +50,45 @@ class Test_delete extends Command
      */
     public function handle()
     {
-        $data = Appoint::where('id' , "!=" , null)->get();
+        $data_Appoint = Appoint::where('id' , "!=" , null)->get();
 
-        foreach ($data as $item) {
+        foreach ($data_Appoint as $item) {
             Appoint::where('id' , $item->id)->delete();
+
+            $data_user  = User::where('id' , $item->patient_id)->first();
+            $provider_id = $data_user->provider_id ;
+
+            $template_path = storage_path('../public/json/text.json');
+            $string_json = file_get_contents($template_path);
+
+            $string_json = str_replace("hello","ผมได้ตรวจสอบแล้ว พบว่าคุณมันรั่วผมเลยลบข้อมูลนัดหมายของคุณ",$string_json);
+
+            $messages = [ json_decode($string_json, true) ];
+            $body = [
+                "to" => $provider_id,
+                "messages" => $messages,
+            ];
+            $opts = [
+                'http' =>[
+                    'method'  => 'POST',
+                    'header'  => "Content-Type: application/json \r\n".
+                                'Authorization: Bearer '.env('CHANNEL_ACCESS_TOKEN'),
+                    'content' => json_encode($body, JSON_UNESCAPED_UNICODE),
+                    //'timeout' => 60
+                ]
+            ];
+
+            $context  = stream_context_create($opts);
+            $url = "https://api.line.me/v2/bot/message/push";
+            $result = file_get_contents($url, false, $context);
+
+            //SAVE LOG
+            $data = [
+                "title" => "ส่งไลน์",
+                "content" => "$provider_id",
+            ];
+
+            Mylog::Create($data);
         }
     }
 
